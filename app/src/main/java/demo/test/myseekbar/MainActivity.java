11ff93a63a81d1cb5063a5b390f17ae9c8d49519
wyb <lishuwentimor1994@163.com>
2019-04-17 09:57:17 +0800

    删除一些文件

--- app/src/main/java/demo/test/myseekbar/MainActivity.java
+++ app/src/main/java/demo/test/myseekbar/MainActivity.java
@@ -2,10 +2,7 @@ package demo.test.myseekbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatSeekBar;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,37 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final ImageView imageView = findViewById(R.id.img);
-        imageView.setOnClickListener(new View.OnClickListener() {
+        SeekBar seek = findViewById(R.id.seek);
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View v) {
-                if (imageView.isClickable()){
-                    imageView.setBackgroundResource(R.drawable.back_rec_blue);
-                }else {
-                    imageView.setBackgroundResource(R.drawable.back_rec_white);
-                }
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                Log.d(TAG, "onProgressChanged: --------");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                seekBar.setThumb(getResources().getDrawable(R.drawable.circle2));
+                Log.d(TAG, "onStartTrackingTouch: --------");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                seekBar.setThumb(getResources().getDrawable(R.drawable.circle1));
+                Log.d(TAG, "onStopTrackingTouch: --------");
             }
         });
-//        SeekBar seek = findViewById(R.id.seek);
-//        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-//            @Override
-//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-//
-//                Log.d(TAG, "onProgressChanged: --------");
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) {
-//                seekBar.setThumb(getResources().getDrawable(R.drawable.circle2));
-//                Log.d(TAG, "onStartTrackingTouch: --------");
-//            }
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) {
-//                seekBar.setThumb(getResources().getDrawable(R.drawable.circle1));
-//                Log.d(TAG, "onStopTrackingTouch: --------");
-//            }
-//        });
     }
 }
